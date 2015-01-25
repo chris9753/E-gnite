108e4e9f4ffbccc5fd4f4758ddafa43c03cc5a4a
Christopher Da Silva <chrisdasilva@Chriss-iMac.local>
2015-01-24 19:21:14 -0500

    Initial Commit

---
+++ E-gnite/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  E-gnite
+//
+//  Created by Chris Da silva on 2015-01-24.
+//  Copyright (c) 2015 Chris Da silva. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
